60d2aee17cd0b63cd484b3b525903ba4bc4e5ad4
jdavidmartinez/git config --global user.name jdavidmartinez <jdavid.martinez@gmail.com>
2021-02-05 16:23:47 -0500

    mnt 45, routes already

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import config from './config';
 
+import  videoRoutes from './routes/Videos.routes' ;
+
 const app= express();
+
+app.use(morgan('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(videoRoutes);
 
 export default app;
 
