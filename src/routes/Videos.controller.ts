d97ea8347b309b7e005043215f3e8a1825450d80
jdavidmartinez/git config --global user.name jdavidmartinez <jdavid.martinez@gmail.com>
2021-02-07 19:52:21 -0500

    mnt 62,validate if the URL is repeatet, it lack to delete de collection in mongo first

--- src/routes/Videos.controller.ts
+++ src/routes/Videos.controller.ts
@@ -1,7 +1,16 @@
 import {RequestHandler} from 'express';
+import Video from './Video';
 
-export const createVideos: RequestHandler = (req, res)=> {
-    res.json('creatting videos')
+export const createVideos: RequestHandler = async (req, res)=> {
+   
+    const videoFound = await Video.findOne({url: req.body.url})
+    if (videoFound)
+    return res.status(301).json({message: 'The URL already exist'});
+
+    const video = new Video(req.body)
+    const savedVideo = await video.save();
+    console.log(savedVideo)
+    res.json('video saved')
    }
 
 export const getVideos: RequestHandler = (req, res)=> {
@@ -9,14 +18,14 @@ export const getVideos: RequestHandler = (req, res)=> {
     }
 
 export const getVideo: RequestHandler = (req, res)=> {
-     res.json('getting videos')
+     res.json('getting a video')
     }
 
 export const deleteVideo: RequestHandler = (req, res)=> {
-     res.json('getting videos')
+     res.json('deletting videos')
     }
 export const updateVideo: RequestHandler = (req, res)=> {
-    res.json('getting videos')
+    res.json('updating a video')
     }
    
 
